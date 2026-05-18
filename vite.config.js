e008af0fd038cae090f9def7330135c8f8df6398
t-santoya <dejesussantiago7912@gmail.com>
2026-05-18 18:10:42 -0500

    PWA instalada

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
+import {
+  defineConfig
+} from 'vite'
 import react from '@vitejs/plugin-react'
+import {
+  VitePWA
+} from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'LUVAUS',
+        short_name: 'LUVAUS',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#0b3f63',
+        theme_color: '#0b3f63',
+        orientation: 'portrait',
+        icons: [{
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
